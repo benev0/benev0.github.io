2791239d0ecb883124ea979c62f3d4bff2c81766
benev0 <benjaminevanownby@gmail.com>
2024-09-05 08:00:12 -0400

    added required components and services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LatexParagraphComponent } from './latex-paragraph/latex-paragraph.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavBarComponent,
+    LatexParagraphComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
