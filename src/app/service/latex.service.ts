f512646a961f2c6e9d475d933039284c78f04afd
benev0 <benjaminevanownby@gmail.com>
2024-09-05 08:02:33 -0400

    added untracked files missed in last commit

---
+++ src/app/service/latex.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import katex, {KatexOptions} from 'katex';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LatexService {
+
+  renderToString(equation: any, options?: KatexOptions): string {
+    return katex.renderToString(equation, options);
+  }
+}
