ee3585340d66a8e1194c6fa02d51009a311352c0
benev0 <benjaminevanownby@gmail.com>
2025-04-26 14:09:18 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environment Gleam";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs,  ...}:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default =
+        pkgs.mkShell {
+          buildInputs = [
+            pkgs.erlang_27
+            pkgs.gleam
+            pkgs.rebar3
+            pkgs.elixir
+            pkgs.glas
+            pkgs.vscode-extensions.gleam.gleam
+          ];
+
+          shellHook = ''
+            echo "Gleam shell ready"
+            gleam --version
+          '';
+        };
+    };
+}
